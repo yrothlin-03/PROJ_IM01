334d9e5e28d36d8b73c3e687d75d897780d6fc50
Yanic Rothlingshofer <yrothlin-24@node27.enst.fr>
2025-11-25 10:06:55 +0100

    test

--- deep/test.py
+++ deep/test.py
@@ -1,7 +1,8 @@
 import torch
 from PIL import Image
 from torchvision.transforms import functional as F
-from .EVSSM import EVSSM
+import torch.nn.functional as Fnn
+from EVSSM import EVSSM
 
 
 
@@ -18,11 +19,16 @@ model.eval()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
+
+
 img = Image.open(f"{img_dir}{img_name}").convert("RGB")   
 x = F.to_tensor(img).unsqueeze(0).to(device) 
-
+print("x.shape before :", x.shape, x.dtype, x.device)
+x = Fnn.interpolate(x, scale_factor=0.5, mode="bilinear", align_corners=False)
+print("x.shape after :", x.shape, x.dtype, x.device)
+print("Inferencing...")
 with torch.no_grad():
-    y = model(x)
+    y = model(x)  
 
 y = torch.clamp(y, 0, 1)
 out = F.to_pil_image(y.squeeze(0).cpu())
